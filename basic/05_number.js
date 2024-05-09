a51c17f80c704b8748d3e908b79a12515bd75fef
Sameer-0077 <165882062+Sameer-0077@users.noreply.github.com>
2024-05-09 07:05:56 +0000

    Num and Math

---
+++ basic/05_number.js
@@ -0,0 +1,32 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(2000)
+console.log(balance);
+
+console.log(typeof(score) + "\n" + typeof(balance));
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const hundreds = 10000000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// ---------------------MATHS-------------------------
+console.log("-----------------------MATHS----------------------");
+
+console.log(Math.abs(-7));
+console.log(Math.pow(3,3));
+console.log(Math.sqrt(36));
+console.log(Math.cbrt(125));
+console.log(Math.round(125.4));
+console.log(Math.ceil(125.4));
+console.log(Math.floor(125.4));
+console.log(Math.PI);
+console.log(Math.SQRT1_2);
+console.log(Math.max(4,3,5,6,8));
+console.log(Math.min(4,3,5,6,8));
+console.log(Math.random);
+console.log(Math.random());
+
+
