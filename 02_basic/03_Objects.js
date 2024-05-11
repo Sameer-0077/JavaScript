3fbf6b44a6134bcae3f5209708eb87e95345ff9d
Sameer-0077 <165882062+Sameer-0077@users.noreply.github.com>
2024-05-11 08:23:18 +0000

    Objects

---
+++ 02_basic/03_Objects.js
@@ -0,0 +1,11 @@
+//Singletone Objects
+//Object.create
+
+//Object Literals
+const JsUser = {
+    name: "Sameer",
+    age: 18,
+    location: "Jaipur",
+    email: "sameer@google.com",
+    isLoggedIn = false
+}
