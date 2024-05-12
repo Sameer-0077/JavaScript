1b89b9b39644e6ccc0616142f73fc7f015db083c
Sameer-0077 <165882062+Sameer-0077@users.noreply.github.com>
2024-05-12 16:34:30 +0000

    Function

---
+++ 03_basic/01_Function.js
@@ -0,0 +1,57 @@
+function printMyName()
+{
+    console.log("S")
+    console.log("A")
+    console.log("M")
+    console.log("E")
+    console.log("E")
+    console.log("R")
+}
+printMyName()
+
+function addTwoNum(num1,num2)
+{
+    console.log(`${num1} + ${num2} = ${num1 + num2}`)
+}
+addTwoNum(5,8)
+
+function returnSqr(num)
+{
+    return num*num
+}
+
+const sqr = returnSqr(4)
+console.log(`Square is ${sqr}`);
+
+/*
+function justLogIn(user)
+{
+    if(!user)
+        {
+            console.log("Please enter your name");
+            return
+        }
+    return `${user} just log in`
+} */
+
+function justLogIn(user = "Sam")
+{
+    return `${user} just log in`
+}
+console.log(justLogIn("Sameer"));
+
+function calculateCartPrice(...num1)
+{
+    return num1
+}
+console.log(calculateCartPrice(200,400,800,1000))
+
+const User = {
+    userName: "Sameer",
+    location:"India"
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.userName} and location is ${anyObject.location}`)
+}
+handleObject(User)
